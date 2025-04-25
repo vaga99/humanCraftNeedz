288bd573ea57992bbcad4f6832e834828263eb28
Valentin MARCEAU <marceauvalentin@gmail.com>
2025-04-25 16:24:39 +0200

    add entities

---
+++ src/Entity/Skill.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SkillRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SkillRepository::class)]
+class Skill
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $label = null;
+
+    /**
+     * @var Collection<int, Need>
+     */
+    #[ORM\ManyToMany(targetEntity: Need::class, inversedBy: 'skills')]
+    private Collection $needs;
+
+    public function __construct()
+    {
+        $this->needs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Need>
+     */
+    public function getNeeds(): Collection
+    {
+        return $this->needs;
+    }
+
+    public function addNeed(Need $need): static
+    {
+        if (!$this->needs->contains($need)) {
+            $this->needs->add($need);
+        }
+
+        return $this;
+    }
+
+    public function removeNeed(Need $need): static
+    {
+        $this->needs->removeElement($need);
+
+        return $this;
+    }
+}
