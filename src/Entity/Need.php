b32a3da5b0c4761f7ad09e2d68ccf6f90bad7c4f
Valentin MARCEAU <marceauvalentin@gmail.com>
2025-04-28 10:28:21 +0200

    add nelmio doc

--- src/Entity/Need.php
+++ src/Entity/Need.php
@@ -20,17 +20,17 @@ class Need
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(["getNeeds", "getNeed"])]
+    #[Groups(["getNeeds", "getNeed", "createNeed"])]
     #[Assert\NotBlank(message: "Title is mandatory")]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
-    #[Groups(["getNeed"])]
+    #[Groups(["getNeed", "createNeed"])]
     #[Assert\NotBlank(message: "Summary is mandatory")]
     private ?string $summary = null;
 
     #[ORM\Column(type: Types::TEXT)]
-    #[Groups(["getNeed"])]
+    #[Groups(["getNeed", "createNeed"])]
     #[Assert\NotBlank(message: "Url is mandatory")]
     #[Assert\Url(message: "Url is not valid")]
     private ?string $url = null;
@@ -39,13 +39,13 @@ class Need
      * @var Collection<int, Skill>
      */
     #[ORM\ManyToMany(targetEntity: Skill::class, mappedBy: 'needs')]
-    #[Groups(["getNeeds", "getNeed"])]
+    #[Groups(["getNeeds", "getNeed", "createNeed"])]
     #[Assert\Count(min: "1")]
     private Collection $skills;
 
     #[ORM\ManyToOne(inversedBy: 'needs')]
     #[ORM\JoinColumn(nullable: false)]
-    #[Groups(["getNeeds", "getNeed"])]
+    #[Groups(["getNeeds", "getNeed", "createNeed"])]
     #[Assert\NotBlank(message: "Author is mandatory")]
     private ?Author $author = null;
 
